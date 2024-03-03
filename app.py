0a6150f23f10a0795fe136f9113a66c2b152b7d1
Z-67 <zayn2830@gmail.com>
2024-03-03 14:12:51 +0000

    added html to app

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)#helps flask know where the flask files are
 
 @app.route("/")#home executed when the root URL ("/") of the web application is accessed
 def home():
-    return "hello world"
+    return render_template("index.html")
 
 # Check if the script is the main program being run and not imported
 if __name__ == "__main__":
